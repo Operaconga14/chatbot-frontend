55eda28e80f2419e036f45adbf1323751de50ea0
Amire Joseph <amirejoseph83@gmail.com>
2025-05-23 22:02:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import axios from "axios"
 import ChatBot from "./components/ChatBot"
 import { NormalButton } from "./shared/Buttons"
-import "axios"
-import { environment } from "./assets/environments/environments"
-// import { useState } from "react"
+
 
 
 function App() {
-
-  // const [isChatOpen, setIsChatOpen] = useState(false);
-
-  // const openChat = () => { setIsChatOpen(true) }
-
-  // const closeChat = () => setIsChatOpen(false);
-
-
-  const handleSessionId = async () => {
-    const api_url = environment.API_URL
-
-    try {
-
-      // await openChat()
-
-      const response = await axios.post(`${api_url}/chat`)
-
-      console.log("Datat", response.data)
-
-      localStorage.setItem("sessiion_id", response.data)
-
-    } catch (error) {
-      console.error("Error", error)
-    }
-
-
-  }
-
   return (
     <>
       <div className="trigger-button">
-        <NormalButton text="💬" className="chabot-trgger-btn" onClick={() => handleSessionId()} />
+        <NormalButton text="💬" className="chabot-trgger-btn" onClick={() => { }} />
       </div>
       <div className="chat-container">
         <ChatBot />
